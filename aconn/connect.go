236f5ccc3591ec965d5d58fb66bf9c9dd53a5a1c
Alex H. Grant <grantal@ravioli.reed.edu>
2016-12-06 14:59:23 -0800

    put installation instructions in connect.go

--- aconn/connect.go
+++ aconn/connect.go
@@ -2,6 +2,11 @@ package aconn
 // connect.go
 // contains functions managing how servers connect and register
 // on the arcade and how clients find servers on the arcade 
+// *** INSTALLATION ***
+// in order for this file to be found, the project will need to be
+// in your $GOPATH/src
+// So, nim_c.go will be at $GOPATH/src/arcade/nim_c.go and this file
+// will be at $GOPATH/src/arcade/aconn/connect.go
 
 
 // hardcoded arcade address
